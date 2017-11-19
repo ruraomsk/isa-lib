08181da35dc9751ed6d6bbb47ee88cac41570b64
rura <rura@rurawork>
2017-11-19 20:36:22 +0600

    Убираем исполняемы

--- raid/pq_check.c
+++ raid/pq_check.c
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-#include "raid.h"
 
+#include "raid.h"
 #include <limits.h>
 #include <stdint.h>
 
