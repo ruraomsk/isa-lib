ab2d47b82bd68c403841feab5feb44b830b29582
rura <rura@rurawork>
2017-11-19 12:43:22 +0600

    speed it

--- raid/xor_check.c
+++ raid/xor_check.c
@@ -8,21 +8,25 @@
 #include <stdint.h>
 #include <stdio.h>
 
-
-int xor_check(int vects, int len, void **array) {
+int xor_check(int vects, int len, void **array)
+{
     int i, j, fail = 0;
-    if (vects < 3) return -1;
-    if (!(len & (sizeof (unsigned long) - 1))) {
-        // выровнено 
+    if (vects < 3)
+        return -1;
+    if (!(len & (sizeof(unsigned long) - 1)))
+    {
+        // выровнено
         unsigned long parity;
-        unsigned long **src = (unsigned long **) array;
-        len = len / sizeof (unsigned long);
+        unsigned long **src = (unsigned long **)array;
+        len = len / sizeof(unsigned long);
         parity = 0;
-        for (i = 0; i < len; i++) {
+        for (i = 0; i < len; i++)
+        {
             for (j = 0; j < vects; j++)
                 parity ^= src[j][i];
-            if (parity != 0) {
-                fail = i+1;
+            if (parity != 0)
+            {
+                fail = i + 1;
                 break;
             }
         }
@@ -30,18 +34,19 @@ int xor_check(int vects, int len, void **array) {
     }
 
     unsigned char parity;
-    unsigned char **src = (unsigned char **) array;
+    unsigned char **src = (unsigned char **)array;
 
-    for (i = 0; i < len; i++) {
+    for (i = 0; i < len; i++)
+    {
         parity = 0;
         for (j = 0; j < vects; j++)
             parity ^= src[j][i];
 
-        if (parity != 0) {
-            fail = i+1;
+        if (parity != 0)
+        {
+            fail = i + 1;
             break;
         }
     }
     return fail;
 }
-
--- raid/xor_gen.c
+++ raid/xor_gen.c
@@ -2,34 +2,54 @@
 #include <limits.h>
 #include <stdint.h>
 
-int xor_gen(int vects, int len, void **array) {
+int xor_gen(int vects, int len, void **array)
+{
     int i, j;
-    if (vects < 3) return -1;
-    if (!(len & (sizeof (unsigned long) - 1))) {
+    if (vects < 3)
+        return -1;
+    if (!(len & (sizeof(unsigned long) - 1)))
+    {
         // выровнено
         unsigned long *pSrc, *pRez, *tmp; //parity;
-        unsigned long **src = (unsigned long **) array;
-        len = len / sizeof (unsigned long);
+        unsigned long **src = (unsigned long **)array;
+        len = len / sizeof(unsigned long);
         // перепишем первую строку в результат
         tmp = src[vects - 1];
-        for (pSrc = src[0], pRez = tmp, i = 0; i < len; i++)
+        i = len;
+        pSrc = src[0];
+        pRez = tmp;
+        while (i--)
             *pRez++ = *pSrc++;
-        //       xor  по строкам
-        for (j = 1; j < vects - 1; j++)
-            for (pSrc = src[j], pRez = tmp, i = 0; i < len; i++)
+         for (j = 1; j < vects - 1; j++)
+        {
+            pSrc = src[j];
+            pRez = tmp;
+            i = len;
+            while (i--)
                 *pRez++ ^= *pSrc++;
+        }
         return 0;
     }
 
     unsigned char *pSrc, *pRez, *tmp; //parity;
-    unsigned char **src = (unsigned char **) array;
+    unsigned char **src = (unsigned char **)array;
 
+    // перепишем первую строку в результат
     tmp = src[vects - 1];
-    for (pSrc = src[0], pRez = tmp, i = 0; i < len; i++)
+    i = len;
+    pSrc = src[0];
+    pRez = tmp;
+    while (i--)
         *pRez++ = *pSrc++;
-    //       xor  по строкам
+
     for (j = 1; j < vects - 1; j++)
-        for (pSrc = src[j], pRez = tmp, i = 0; i < len; i++ )
+    {
+        pSrc = src[j];
+        pRez = tmp;
+        i = len;
+        while (i--)
             *pRez++ ^= *pSrc++;
+    }
     return 0;
 }
+
